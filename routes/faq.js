54a2ca3ff6ea84450316b3cbcd9b262692ed21c3
Pranav <khandagalepranav1546@gmail.com>
2025-02-01 00:41:48 +0530

    feat: get route, fetches FAQ as per lang or else english by default

--- routes/faq.js
+++ routes/faq.js
@@ -1,6 +1,6 @@
 const Router = require('express');
 const { translateTo, supportedLanguages } = require('../utils/utils');
-const { faqModel } = require('../db');
+const { faqModel } = require('../database/db');
 
 const faqRouter = Router();
 
@@ -34,6 +34,27 @@ faqRouter.post("/add-faq", async (req, res) => {
         res.status(500).json({
             message: "Error adding FAQ" + e,
         })
+        return;
+    }
+})
+
+faqRouter.get("/get-faq", async (req, res)=>{
+    const lang = req.query.lang;
+
+    try {
+        const faqs = await faqModel.find();
+        const translatedFAQS = faqs.map(faq => ({
+            question: faq.translations[lang]?.question || faq.question,
+            answer: faq.translations[lang]?.answer || faq.answer,
+        }));
+        res.status(200).json({
+            translatedFAQS
+        });
+        return;
+    } catch(e) {
+        res.status(500).json({
+            message: "Error fetching FAQs "+e,
+        });
         return;
     }
 })
